b9b4b1dc05e618f44367a1d5e2b96c5c40f1327c
brameshit9 <41820823+brameshit9@users.noreply.github.com>
2026-09-21 20:26:28 -0700

    Update app.py

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ import plotly.graph_objects as go
 # -------------------------------------------------------------------
 st.set_page_config(page_title="Nifty50 Premarket Tracker", layout="wide")
 
-KEY_OPTIONS = ["NIFTY 50", "NIFTY BANK", "SECURITIES IN F&O" "ALL"]
+KEY_OPTIONS = ["NIFTY 50", "NIFTY BANK", "SECURITIES IN F&O", "ALL"]
 
 
 # -------------------------------------------------------------------
